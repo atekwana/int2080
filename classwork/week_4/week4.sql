492d2c89cb0c8de996f871b94a848b22c440e750
kasey <90230268+atekwana@users.noreply.github.com>
2025-09-10 22:03:13 -0400

    Update week4.sql

--- classwork/week_4/week4.sql
+++ classwork/week_4/week4.sql
@@ -1,4 +1,7 @@
+drop table if exists classwork.child_1;
+drop table if exists classwork.child_2;
 drop table if exists classwork.parent;
+
 -- creating the father table
 create table classwork.parent (
 	last_name VARCHAR(50) not null,
@@ -15,7 +18,6 @@ from
 	classwork.parent
 as last_name;
 
-drop table if exists classwork.child_1;
 -- create child table
 create table classwork.child_1 (
 	last_name VARCHAR(50) not null,
@@ -32,7 +34,6 @@ from
 	classwork.child_1
 as last_name;
 
-drop table if exists classwork.child_2;
 -- create child table
 create table classwork.child_2 (
 	last_name VARCHAR(50) not null,
