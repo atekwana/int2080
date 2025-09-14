b59dc9befbdf7c23bbe199ef742ec39db0deebf2
atekwana <kaseyatekwana@protonmail.com>
2025-09-14 19:05:23 -0400

    Formatting

---
+++ labs/lab_4/er-schema.sql
@@ -0,0 +1,9 @@
+-- dropping student table(s)
+drop table if exists student.author;
+drop table if exists student.customers;
+drop table if exists student.publisher;
+
+-- show table
+use student.author;
+
+
